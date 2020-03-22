af603738c9c40dd797eaf6d4fbba025b1e623800
Mazilu Andrei <dei.andrei98@gmail.com>
2020-03-22 17:59:24 +0200

    Character Controller

---
+++ UnityProject/Assets/PlayerMouseLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouseLook : MonoBehaviour
+{
+    public const float m_MouseSensitivity = 500.0f;
+
+    public Transform m_playerBody;
+    float xRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * m_MouseSensitivity * Time.deltaTime;
+        
+        float mouseY = Input.GetAxis("Mouse Y") * m_MouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0.0f, 0.0f);
+        
+        m_playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
