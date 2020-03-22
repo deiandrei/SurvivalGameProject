934124013dd4497ce23362926e7cf81c7a9a8b2d
Mazilu Andrei <dei.andrei98@gmail.com>
2020-03-22 18:39:13 +0200

    Updates to the Character Controller

--- UnityProject/Assets/PlayerMovement.cs
+++ UnityProject/Assets/PlayerMovement.cs
@@ -5,23 +5,41 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController m_Controller;
+    public Transform m_GroundCheck;
+
+    public LayerMask m_GroundMask;
 
     public float Speed = 12.0f;
+    public float Gravity = -9.81f;
+    public float GroundDistance = 0.4f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float JumpHeight = 3.0f;
+
+    Vector3 Velocity;
+    bool isGroundHit;
 
     // Update is called once per frame
     void Update()
     {
+        isGroundHit = Physics.CheckSphere(m_GroundCheck.position, GroundDistance, m_GroundMask);
+
+        if(isGroundHit && Velocity.y < 0)
+        {
+            Velocity.y = -1.0f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
+        m_Controller.Move(move * Speed * Time.deltaTime);
 
-        m_Controller.Move(move * 12.0f * Time.deltaTime);
+        if(Input.GetButtonDown("Jump"))
+        {
+            Velocity.y = Mathf.Sqrt(JumpHeight * -2.0f * Gravity);
+        }
+
+        Velocity.y += Gravity * Time.deltaTime;
+        m_Controller.Move(Velocity * Time.deltaTime);
     }
 }
